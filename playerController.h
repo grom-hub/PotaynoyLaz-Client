11d5fd16f40e389517c771bffdbebe4f8e903098
tixiydrakon <ya4ule@gmail.com>
2019-05-08 22:09:34 +0300

    start

---
+++ playerController.h
@@ -0,0 +1,28 @@
+#pragma once
+
+//#include <string>
+//#include <vector>
+
+#include "connector.h"
+#include "ncScreen.h" // PrintData, StatusData
+
+
+
+class PlayerController
+{
+public:
+	PlayerController();
+	int createPlayer(Connector &connect, int &myid, CreateData &createData);
+	void setCommand(int input, int &myid, char *sendBuff, int &sendSize);
+	void recvBuffHandler(char *recvBuff, NcScreen &screen);
+
+
+
+
+private:
+    int unitsFrameNum;
+    int pwrPointsFrameNum;
+
+	int printObjectsSize[3];
+
+};
